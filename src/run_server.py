8cecf072678950e000f4d5198f551abb5338a57b
aymeP <58934999+aymeP@users.noreply.github.com>
2021-06-17 17:55:50 +0200

    Ajout page de connexion

--- src/run_server.py
+++ src/run_server.py
@@ -9,7 +9,7 @@ Modified on April 2021
 
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 # définir le message secret
 import build
@@ -20,11 +20,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def get_secret_message():
-    return SECRET_MESSAGE
+    #return SECRET_MESSAGE
+    return render_template("connexion.html")
+
 
 
 if __name__ == "__main__":
     # HTTP version
-    #app.run(debug=True, host="0.0.0.0", port=8081)
+    app.run(debug=True, host="0.0.0.0", port=8081)
     # HTTPS version
-    app.run(debug=True, host="0.0.0.0", port=8081,ssl_context=(build.SERVER_PUBLIC_KEY_FILENAME, build.SERVER_PRIVATE_KEY_FILENAME))
+    #app.run(debug=True, host="0.0.0.0", port=8081,ssl_context=(build.SERVER_PUBLIC_KEY_FILENAME, build.SERVER_PRIVATE_KEY_FILENAME))
