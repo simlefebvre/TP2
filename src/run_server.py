c1559cb5ba19ef0ce4a122062b80456a7a4dab06
aymeP <58934999+aymeP@users.noreply.github.com>
2021-06-17 10:52:12 +0200

    Update run_server.py

--- src/run_server.py
+++ src/run_server.py
@@ -11,17 +11,9 @@ Modified on April 2021
 
 from flask import Flask
 
-RESOURCES_DIR = "resources/"
-CA_PRIVATE_KEY_FILENAME = RESOURCES_DIR + "ca-private-key.pem"
-CA_PUBLIC_KEY_FILENAME = RESOURCES_DIR + "ca-public-key.pem"
-SERVER_PRIVATE_KEY_FILENAME = RESOURCES_DIR + "server-private-key.pem"
-SERVER_CSR_FILENAME = RESOURCES_DIR + "server-csr.pem"
-SERVER_PUBLIC_KEY_FILENAME = RESOURCES_DIR + "server-public-key.pem"
-
-CERTIFICATE_FILENAME = RESOURCES_DIR + "certif.pem"
-
-
 # définir le message secret
+import build
+
 SECRET_MESSAGE = "arbre" # A modifier
 app = Flask(__name__)
 
@@ -35,4 +27,4 @@ if __name__ == "__main__":
     # HTTP version
     #app.run(debug=True, host="0.0.0.0", port=8081)
     # HTTPS version
-    app.run(debug=True, host="0.0.0.0", port=8081,ssl_context=(SERVER_PUBLIC_KEY_FILENAME, SERVER_PRIVATE_KEY_FILENAME))
+    app.run(debug=True, host="0.0.0.0", port=8081,ssl_context=(build.SERVER_PUBLIC_KEY_FILENAME, build.SERVER_PRIVATE_KEY_FILENAME))
